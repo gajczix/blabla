a5cbbd935ccce04bcbbf65420dc762327d1b70c0
Krystyna Gajczyk <kg332118@students.mimuw.edu.pl>
2016-01-04 13:10:07 +0100

    cos dziala

---
+++ priorityqueue.hh
@@ -0,0 +1,124 @@
+#include <map>
+#include <cstdlib>
+#include <utility>
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+template <typename K>
+struct classcomp {
+  bool operator() (const K * lhs, const K * rhs) const
+  {return *lhs < *rhs;}
+};
+
+
+template <typename K, typename V> 
+class PriorityQueue {
+	
+	std::multimap <V, K> values;
+	std::map <K*, typename std::multimap <V, K>::iterator , classcomp<K> > iterators;
+	
+	public:
+	// Konstruktor bezparametrowy tworzący pustą kolejkę [O(1)]
+	PriorityQueue(){}
+
+	// Konstruktor kopiujący [O(queue.size())]
+	PriorityQueue(const PriorityQueue<K, V>& queue);
+
+	// Konstruktor przenoszący [O(1)]
+	PriorityQueue(PriorityQueue<K, V>&& queue);
+
+	// Operator przypisania [O(queue.size()) dla użycia P = Q, a O(1) dla użycia
+	// P = move(Q)]
+	PriorityQueue<K, V>& operator=(PriorityQueue<K, V> queue);
+
+	// Metoda zwracająca true wtedy i tylko wtedy, gdy kolejka jest pusta [O(1)]
+	bool empty() const{
+		return values.empty();
+	}
+
+	// Metoda zwracająca liczbę par (klucz, wartość) przechowywanych w kolejce
+	// [O(1)]
+	std::size_t size() const{ //TODO: poprawic na size_type
+		return values.size();
+	}
+
+	// Metoda wstawiająca do kolejki parę o kluczu key i wartości value
+	// [O(log size())] (dopuszczamy możliwość występowania w kolejce wielu
+	// par o tym samym kluczu)
+	void insert(const K& key, const V& value){
+		auto it = values.emplace(value, key);
+		auto elem = &it->second;
+		iterators.emplace(elem, it);
+	}
+	
+
+	// Metody zwracające odpowiednio najmniejszą i największą wartość przechowywaną
+	// w kolejce [O(1)]; w przypadku wywołania którejś z tych metod na pustej
+	// strukturze powinien zostać zgłoszony wyjątek PriorityQueueEmptyException
+	const V& minValue() const{
+		return values.begin()->first;
+	}	
+	const V& maxValue() const{
+		return values.rbegin()->first;
+	}
+
+	// Metody zwracające klucz o przypisanej odpowiednio najmniejszej lub
+	// największej wartości [O(1)]; w przypadku wywołania którejś z tych metod
+	// na pustej strukturze powinien zostać zgłoszony wyjątek
+	// PriorityQueueEmptyException
+	const K& minKey() const{
+		return values.begin()->second;
+	}
+	const K& maxKey() const{
+		return values.rbegin()->second;
+	}
+	private:
+	void deleteElem(const K& key){
+		K nowe = key;
+		K * wskaznik = &nowe;
+		auto it = iterators.find(wskaznik);
+		values.erase(it->second);
+		iterators.erase(it);
+	}
+	public:
+	// Metody usuwające z kolejki jedną parę o odpowiednio najmniejszej lub
+	// największej wartości [O(log size())]
+	void deleteMin(){
+		if(values.empty())
+			return;
+		deleteElem(values.begin()->second);
+	}
+	void deleteMax(){
+		if(values.empty())
+			return;
+		deleteElem(values.rbegin()->second);
+	}
+	
+	// Metoda zmieniająca dotychczasową wartość przypisaną kluczowi key na nową
+	// wartość value [O(log size())]; w przypadku gdy w kolejce nie ma pary
+	// o kluczu key, powinien zostać zgłoszony wyjątek
+	// PriorityQueueNotFoundException(); w przypadku kiedy w kolejce jest kilka par
+	// o kluczu key, zmienia wartość w dowolnie wybranej parze o podanym kluczu
+	void changeValue(const K& key, const V& value){
+		deleteElem(key);
+		this->insert(key, value);
+	}
+	
+	// Metoda scalająca zawartość kolejki z podaną kolejką queue; ta operacja usuwa
+	// wszystkie elementy z kolejki queue i wstawia je do kolejki *this
+	// [O(queue.size() * log (queue.size() + size()))]
+	void merge(PriorityQueue<K, V>& queue){
+		for(auto elem : queue.values){
+			this->insert(elem.second->first, elem.first);
+			queue.delete_elem(elem.second);
+		}
+	}
+
+	// Metoda zamieniającą zawartość kolejki z podaną kolejką queue (tak jak
+	// większość kontenerów w bibliotece standardowej) [O(1)]
+	void swap(PriorityQueue<K, V>& queue){
+		
+	}
+};
---
+++ priotest2.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <exception>
+#include <cassert>
+
+#include "priorityqueue_exp2.h"
+
+
+int main() {
+    PriorityQueue<int, int> P;
+    assert(P.empty());
+
+	P.insert(3, 24);
+    P.insert(1, 42);
+    P.insert(2, 13);
+
+	P.changeValue(2, 14);
+	
+    assert(P.size() == 3);
+    assert(P.maxKey() == 1);
+    assert(P.maxValue() == 42);
+    assert(P.minKey() == 2);
+    assert(P.minValue() == 14);
+	
+	
+	P.deleteMax();
+    P.deleteMin();
+    P.deleteMin();
+
+    assert(P.empty());
+}
+
+
+/*
+
+#include <iostream>
+#include <exception>
+#include <cassert>
+
+#include "priorityqueue.hh"
+
+PriorityQueue<int, int> f(PriorityQueue<int, int> q)
+{
+    return q;
+}
+
+int main() {
+    PriorityQueue<int, int> P;
+    assert(P.empty());
+
+    P.insert(1, 42);
+    P.insert(2, 13);
+
+    assert(P.size() == 2);
+    assert(P.maxKey() == 1);
+    assert(P.maxValue() == 42);
+    assert(P.minKey() == 2);
+    assert(P.minValue() == 13);
+}
+    PriorityQueue<int, int> Q(f(P));
+   Q.deleteMax();
+    Q.deleteMin();
+    Q.deleteMin();
+
+    assert(Q.empty());
+
+    PriorityQueue<int, int> R(Q);
+
+    R.insert(1, 100);
+    R.insert(2, 100);
+    R.insert(3, 300);
+
+    PriorityQueue<int, int> S;
+    S = R;
+
+    try
+    {
+        S.changeValue(4, 400);
+    }
+    catch (const PriorityQueueNotFoundException& pqnfex)
+    {
+        std::cout << pqnfex.what() << std::endl;
+    }
+    catch (...)
+    {
+        assert(!"exception missing!");
+    }
+
+    S.changeValue(2, 200);
+
+    try
+    {
+        while (true)
+        {
+            std::cout << S.minValue() << std::endl;
+            S.deleteMin();
+        }
+        assert(!"S.minValue() on empty S did not throw!");
+    }
+    catch (const PriorityQueueEmptyException& pqeex)
+    {
+        std::cout << pqeex.what() << std::endl;
+    }
+    catch (...)
+    {
+        assert(!"exception missing!");
+    }
+
+    try
+    {
+        S.minKey();
+        assert(!"S.minKey() on empty S did not throw!");
+    }
+    catch (const std::exception& ex)
+    {
+        std::cout << ex.what() << std::endl;
+    }
+    catch (...)
+    {
+        assert(!"exception missing!");
+    }
+
+    PriorityQueue<int, int> T;
+    T.insert(1, 1);
+    T.insert(2, 4);
+    S.insert(3, 9);
+    S.insert(4, 16);
+    S.merge(T);
+    assert(S.size() == 4);
+    assert(S.minValue() == 1);
+    assert(S.maxValue() == 16);
+    assert(T.empty());
+
+    S = R;
+    swap(R, T);
+    assert(T == S);
+    assert(T != R);
+
+    R = std::move(S);
+    assert(T != S);
+    assert(T == R);
+
+    std::cout << "ALL OK!" << std::endl;
+
+    return 0;
+}
+
+UWAGA! Każda implementacja PriorityQueue<K, V> zostanie sprawdzona zestawem
+testów z typami, które np. zgłaszają wyjątki ze swoich metod, operatorów itd.
+(oczywiście poza destruktorem). Symulowane będą też niepowodzenia w alokacji
+pamięci (bad_alloc). Przejście tych testów będzie podstawą do zaliczenia.
+Warto zatem spróbować samemu zasymulować takie warunki. Wskazówka, jak to
+zrobić, znajduje się w jednej z obowiązkowych czytanek.
+
+Ustalenia techniczne
+^^^^^^^^^^^^^^^^^^^^
+Jako rozwiązanie należy dostarczyć plik priorityqueue.hh.
+Plik ten należy umieścić w repozytorium w katalogu
+
+grupaN/zadanie5/ab123456+cd123456
+
+lub
+
+grupaN/zadanie5/ab123456+cd123456+ef123456
+
+gdzie N jest numerem grupy, a ab123456, cd123456, ef123456 są identyfikatorami
+członków zespołu umieszczającego to rozwiązanie. Katalog z rozwiązaniem nie
+powinien zawierać innych plików, ale może zawierać podkatalog private, gdzie
+można umieszczać różne pliki, np. swoje testy. Pliki umieszczone w tym
+podkatalogu nie będą oceniane.
+*/
